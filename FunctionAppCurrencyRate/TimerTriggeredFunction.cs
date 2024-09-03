869f9d11b150d9c82325885e92389c9c7732e274
DESKTOP-KUSCP3I\roman <romannep1989@gmail.com>
2024-09-03 23:26:43 +0300

    init proj

---
+++ FunctionAppCurrencyRate/TimerTriggeredFunction.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppCurrencyRate;
+
+public class TimerTriggeredFunction
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    [FunctionName("TimerTriggeredFunction")]
+    public async Task Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"Timer triggered at: {DateTime.Now}");
+
+        HttpResponseMessage response = await client.GetAsync("https://testapp1989-hydbctbeewbthab4.westeurope-01.azurewebsites.net/WeatherForecast/unfreeze");
+        if (response.IsSuccessStatusCode)
+        {
+            log.LogInformation("Request succeeded.");
+        }
+        else
+        {
+            log.LogError("Request failed.");
+        }
+    }
+}
+
